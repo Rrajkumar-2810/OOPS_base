b2835a842f28be48a596493678623169a1f2fa18
Rrajkumar786 <rishirajkumar28oct@gmail.com>
2024-09-15 00:09:36 +0530

    Update sidd.java

--- sidd.java
+++ sidd.java
@@ -1,7 +1,24 @@
 package OOPS_base;
+import java.util.Scanner;
 
 public class sidd {
+	
+	static public int multiply(int n){
+		return 5*n;
+	}
+	
+	static public int subtract(int a, int b) {
+		return multiply(a-b);
+	}
+	
 	public static void main(String[] args){
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the two numbers: ");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("a: "+a);
+		System.out.println("b: "+b);
+		System.out.println("Result: "+subtract(a,b));
 		
 	}
 }
